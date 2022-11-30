b23696c326bd4005ad300d16f06e6c150057ef8d
RJRYT <robinjratr@gmail.com>
2022-11-30 17:51:52 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Bot Developer", "Samp Dev", "Dc Developer", "Designer"],
+        strings: ["YouTuber", "Bot Developer", "FrontEnd Developer", "Gamer", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Bot Developer", "Samp Dev", "Dc Developer", "Designer"],
+        strings: ["YouTuber", "Bot Developer", "FrontEnd Developer", "Gamer", "Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
